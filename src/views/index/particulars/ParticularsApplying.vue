1e6b7b6fa03aa20a7d138951314cd1f05a31a03e
kanto-luna <kantoluma@gmail.com>
2025-09-25 15:12:30 +0800

    fix(tsconfig) : add defined script to tsconfig including

--- src/views/index/particulars/ParticularsApplying.vue
+++ src/views/index/particulars/ParticularsApplying.vue
@@ -1,41 +1,41 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { NInput } from 'naive-ui'
+import { ref } from "vue"
+import { NInput } from "naive-ui"
 
-import DataTablePanel from '@/components/data/DataTablePanel.vue'
-import type { FormItems, TableProps } from '@/defined/component-prop'
-import type { ParticularApplying } from '@/defined/particulars'
+import DataTablePanel from "@/components/data/DataTablePanel.vue"
+import type { FormItems, TableProps } from "@/defined/component-prop"
+import type { ParticularApplying } from "@/defined/particulars"
 
 const formValues = ref({
-  companyName: '',
-  employeeName: '',
-  status: '',
+  companyName: "",
+  employeeName: "",
+  status: "",
 })
 const formItems = ref<FormItems>([
   {
-    key: 'name',
-    label: '姓名',
+    key: "name",
+    label: "姓名",
     component: NInput,
   },
   {
-    key: 'employeeName',
-    label: '所属销售',
+    key: "employeeName",
+    label: "所属销售",
     component: NInput,
   },
   {
-    key: 'status',
-    label: '状态',
+    key: "status",
+    label: "状态",
     component: NInput,
   }
 ])
 const tableProps = ref<TableProps<ParticularApplying>>({
   columns: [
-    { key: 'companyName', title: '企业名称' },
-    { key: 'employeeName', title: '所属销售' },
-    { key: 'industry', title: '行业' },
-    { key: 'status', title: '状态' },
-    { key: 'createAt', title: '创建时间' },
-    { key: 'operations', title: '操作' },
+    { key: "companyName", title: "企业名称" },
+    { key: "employeeName", title: "所属销售" },
+    { key: "industry", title: "行业" },
+    { key: "status", title: "状态" },
+    { key: "createAt", title: "创建时间" },
+    { key: "operations", title: "操作" },
   ],
   data: [],
   pagination: {
